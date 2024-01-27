5269757b8c301c7ad0e5cb72bcfdb7cd8c8dcaf4
Chris Whidden <cwhidden@dal.ca>
2024-01-27 03:04:38 -0400

    Initial commit

---
+++ test/MatrixTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/* Test failure record:
+ * 1.   [Name of test method]  [Method that failed]   [Notes]
+ * 2.
+ * 3.
+ * 4.
+ * 5.
+ *  ...
+ */
+class MatrixTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void negate() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiplyWithScalar() {
+    }
+
+    @Test
+    void multiplyWithMatrix() {
+    }
+
+    @Test
+    void getElem() {
+    }
+
+    @Test
+    void setElem() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+}
